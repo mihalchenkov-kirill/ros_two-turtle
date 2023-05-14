6d8e3908393accef6cb1346da27ea20a5650aff7
mihalchenkov_kirill <mihalchenkov.ku@gmail.com>
2023-05-14 20:29:40 +0300

    add project

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/dova/homework/src
+# Build directory: /home/dova/homework/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("turtle_follower")
---
+++ build/turtle_follower/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/dova/homework/src/turtle_follower
+# Build directory: /home/dova/homework/build/turtle_follower
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
